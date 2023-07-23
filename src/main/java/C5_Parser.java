deb9e071a01115f2ad2a9cf3fedebdba7a6af86a
Anton V. Kirilchik <kosmonaffft@gmail.com>
2023-07-23 13:52:49 +0300

    Commit 3.

--- src/main/java/C5_Parser.java
+++ src/main/java/C5_Parser.java
@@ -6,7 +6,8 @@ public class C5_Parser {
     private static final String dn = "CN=Ivanov Ivan Ivanovich,ADDR=Russia, Tatarstan rep., Kazan city, Br. Kasimovykh str.,bld. 45,OU=Development,DC=cbr,DC=ru";
 
     public static void main(String[] args) {
-        parse(dn);
+        List<Map.Entry<String, String>> parsed = parse(dn);
+        System.out.println(parsed);
     }
 
     private static List<Map.Entry<String, String>> parse(String dn) {
