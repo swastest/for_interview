56bf1ae5840d59fbc428b04f30a7e6e3524d9c57
Anton V. Kirilchik <kosmonaffft@gmail.com>
2024-01-31 17:13:57 +0300

    Update C1_Compariosn.js
    Answers below questions.

--- src/main/js/C1_Compariosn.js
+++ src/main/js/C1_Compariosn.js
@@ -1,10 +1,21 @@
-console.log(3 > 2 > 1); // false
+console.log(3 > 2 > 1);
 
-console.log(null == undefined); // true
-console.log(null === undefined); // true
+console.log(null == undefined);
+console.log(null === undefined);
 
-console.log(false == '0'); // true
+console.log(false == '0');
 
-console.log(0.1 + 0.2 == 0.3); // false
+console.log(0.1 + 0.2 == 0.3);
 
-console.log(typeof typeof 1); // string
+console.log(typeof typeof 1);
+
+// false
+
+// true
+// false
+
+// true
+
+// false
+
+// 'string'
